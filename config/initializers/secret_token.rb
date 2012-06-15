ca031fa8d974d4201bdfe8edcab93a50379c950b
Cakey | Buddy Magsipoc <keikun17@gmail.com>
2012-06-16 00:03:27 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bestpractice::Application.config.secret_token = '3236f17829facf059c2f64876b7ee4984abc71aca56a22a6f070d654de7ae9ad8cffe9e5e718a55dfb426e1fb20b6d42639324ccee1a98cdbfb96e2a2629e916'
